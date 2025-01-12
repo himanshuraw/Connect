3c803679fe47273f1c824e974234d0f9e5df203e
himanshuraw <himanshurawat180@gmail.com>
2025-01-12 23:58:40 +0530

    created PostService : createPost and DeletePost

---
+++ backend/src/services/post.service.ts
@@ -0,0 +1,51 @@
+import { error } from "console";
+import { AppDataSource } from "../config/db";
+import { Post } from "../models/postgresql/Post";
+import { User } from "../models/postgresql/User";
+import { PostData } from "../types/PostData";
+
+export class PostService {
+    private static postRepository = AppDataSource.getRepository(Post);
+    private static userRepository = AppDataSource.getRepository(User);
+
+    static async getAllPost() {
+        return this.postRepository.find({
+            relations: ["author", "comments", "likes"],
+            order: { createdAt: "DESC" },
+        });
+    }
+
+    static async getPostByUser(id: number) {
+        return this.postRepository.find({
+            where: { author: { id } },
+            relations: ["author", "comments", "likes"],
+            order: { createdAt: "DESC" },
+        });
+    }
+
+    static async createPost(data: PostData) {
+        const { authorId, imageUrl, caption, tags } = data;
+
+        const author = await this.userRepository.findOne({
+            where: { id: authorId }
+        });
+
+        if (!author) {
+            throw new Error(`Author with ID ${authorId} not found`);
+        }
+
+        const newPost = this.postRepository.create({
+            author,
+            imageUrl,
+            caption,
+            tags,
+        })
+
+        return await this.postRepository.save(newPost);
+    }
+
+    static async deletePostById(id: number) {
+        const result = await this.postRepository.delete(id);
+        return result.affected! > 0;
+    }
+}
---
+++ backend/src/types/PostData.ts
@@ -0,0 +1,6 @@
+export interface PostData {
+    authorId: number;
+    imageUrl: string;
+    caption?: string;
+    tags?: string[];
+}
