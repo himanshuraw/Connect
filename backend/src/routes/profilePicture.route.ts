4f3e6024268cb968426f46e006f455bd05920b5d
himanshuraw <himanshurawat180@gmail.com>
2025-01-12 15:28:41 +0530

    adjusted param issue

--- backend/src/routes/profilePicture.route.ts
+++ backend/src/routes/profilePicture.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
 router.post('/', UserController.addProfilePicture);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', userRoutes);
+app.use('/users', userRoutes);
 
 // Database connections
 (async () => {
